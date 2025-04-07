24e4d1518e93460809f6785f7a1ee4d1daaad48d
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-04-07 21:26:25 +0800

    exploring Action Parameters

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,11 +14,29 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek" };
 
-            //return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy = "name"});
+            return View(movie);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0} & sortBy={1}", pageIndex, sortBy));
         }
     }
 }
