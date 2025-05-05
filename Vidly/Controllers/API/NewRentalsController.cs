f72687eaf4d4d69c71aabd4d20575e2505c1855a
Dan Mark Villaplana <dan.mark.villaplana@gmail.com>
2025-05-05 15:02:28 +0800

    adding the API implementation for the NewRental

--- Vidly/Controllers/API/NewRentalsController.cs
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -15,15 +15,31 @@ namespace Vidly.Controllers.API
     {
         private ApplicationDbContext _context;
 
-        public NewRentalsController(ApplicationDbContext context)
+        public NewRentalsController()
         {
-            _context = context;
+            _context = new ApplicationDbContext();
         }
 
         [HttpPost]
         public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customers.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now,
+                };
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
         }
 
         //public IHttpActionResult GetCustomer(int id)
